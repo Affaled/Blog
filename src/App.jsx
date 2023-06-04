6fbaaaaa686b7e46ce965b66280ed95f7ca27a3a
Affaled <120531449+Affaled@users.noreply.github.com>
2023-06-04 02:45:58 -0300

    ajuste para renderizar header e navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import "./assets/styles/App.css";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>
+  );
 }
