a3e19e468570534dc0bb76daf24b0ef7e568671d
Affaled <120531449+Affaled@users.noreply.github.com>
2023-06-04 10:22:36 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "../assets/styles/Navbar.css";
 
 export default function Navbar() {
   return (
-    <div>
-      <p>ABOUT</p>
-      <p>HOME</p>
-    </div>
+    <nav className="navbar">
+      <a href="#">ABOUT</a>
+      <a href="#">HOME</a>
+      <a href="#">GALERY</a>
+      <a href="#">PORTFOLIO</a>
+      <a href="#">CATEGORIES</a>
+      <a href="#">SHOP</a>
+    </nav>
   );
 }
