ac49382e83cd53e0ea45e3d83cf6a480d10ec507
Affaled <120531449+Affaled@users.noreply.github.com>
2023-06-08 09:24:08 -0300

    recomeço

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
-import React from "react";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 import "./assets/styles/App.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div>
-        <Header />
-        <Navbar />
-      </div>
-      <div className="container">
-        <Home />
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
