2df095aa49626a92ea51b6361374d4b99a758587
Affaled <120531449+Affaled@users.noreply.github.com>
2023-06-09 19:41:08 -0300

    criação da navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,29 @@ import "../assets/styles/Header.css";
 
 export default function Header() {
   return (
-    <div>
-      <h1>Lucar Affaled</h1>
-    </div>
+    <header className="header">
+      <div className="header_title">
+        <h1>Lucar Affaled</h1>
+      </div>
+      <nav className="header_nav">
+        <ul className="header_link_list">
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href="#">ABOUT</a>
+          </li>
+          <li>
+            <a href="#">JOURNAL</a>
+          </li>
+          <li>
+            <a href="#">CONTACT</a>
+          </li>
+          <li>
+            <a href="#">GALLERY</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
