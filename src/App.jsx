5e5e6f703bf6df64f617a4332a435b5bd172cc84
Affaled <120531449+Affaled@users.noreply.github.com>
2023-06-09 19:14:35 -0300

    implementação do Header para testes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
 import "./assets/styles/App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
