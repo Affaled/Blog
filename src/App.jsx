38870ef4f65eb6ede82868a48db196fa7a3e56bd
Affaled <120531449+Affaled@users.noreply.github.com>
2023-06-17 15:00:07 -0300

    reorganização do App na pasta components

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import "./assets/styles/App.css";
-
-export default function App() {
-  return <div className="App"></div>;
-}
---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "./Header";
+import Home from "../pages/Home";
+import "../assets/styles/App.css";
+
+export default function App() {
+  return <div className="App"></div>;
+}
