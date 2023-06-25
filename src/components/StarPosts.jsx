8dae585b455ce3ed7963e7cbfb636821ef4a66cb
Affaled <120531449+Affaled@users.noreply.github.com>
2023-06-24 21:26:24 -0300

    adição da barra de informações do post

--- src/components/StarPosts.jsx
+++ src/components/StarPosts.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import img1 from "../assets/images/img1.jpg";
 import "../assets/styles/StarPosts.css";
+import "../assets/styles/StarPost-Info.css";
 
 export default function StarPosts() {
   return (
     <div className="starposts_container">
       <img src={img1} alt="img1" />
+      <div className="info_container">
+        <a href="">Category</a>
+        <a href="">Date</a>
+        <a href="">Hour</a>
+        <a href="">Read More</a>
+      </div>
     </div>
   );
 }
