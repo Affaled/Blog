1c87e72c560e9715de01256963180e84e6a1d053
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-20 03:26:42 -0300

    separação de conteúdo e criação de novo componente

--- src/components/home/home.module.ts
+++ src/components/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
+import { MainNoticeComponent } from '../mainNotice/mainNotice.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, MainNoticeComponent],
   imports: [CommonModule],
-  exports: [HomeComponent],
+  exports: [HomeComponent, MainNoticeComponent],
 })
 export class HomeModule {}
---
+++ src/components/mainNotice/mainNotice.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-notice',
+  templateUrl: './mainNotice.component.html',
+  styleUrls: ['./mainNotice.component.scss'],
+})
+export class MainNoticeComponent {}
