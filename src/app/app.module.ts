2e895f2e2f0b6d020e734bae6b33618dc9801947
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-20 13:54:04 -0300

    rotas criadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderModule } from 'src/components/header/header.module';
 import { HomeModule } from 'src/components/home/home.module';
+import { ContentModule } from 'src/components/content/content.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeaderModule, HomeModule],
+  imports: [
+    BrowserModule,
+    HeaderModule,
+    HomeModule,
+    ContentModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
