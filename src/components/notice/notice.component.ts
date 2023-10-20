14027e98f326e7a5ba386b9a3ab26c79f88908e4
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-20 09:48:25 -0300

    criação notice component

---
+++ src/components/notice/notice.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notice',
+  templateUrl: './notice.component.html',
+  styleUrls: ['./notice.component.scss'],
+})
+export class NoticeComponent {}
