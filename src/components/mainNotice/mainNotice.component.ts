95533b6e1dd0a6d6331d64eca4e19ddc7f0e6711
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-20 12:24:57 -0300

    adicionado acesso a dados

--- src/components/mainNotice/mainNotice.component.ts
+++ src/components/mainNotice/mainNotice.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-notice',
   templateUrl: './mainNotice.component.html',
   styleUrls: ['./mainNotice.component.scss'],
 })
-export class MainNoticeComponent {}
+export class MainNoticeComponent implements OnInit {
+  @Input()
+  noticePhoto: string = '';
+  @Input()
+  noticeTitle: string = '';
+  @Input()
+  noticeContent: string = '';
+
+  constructor() {}
+  ngOnInit(): void {}
+}
--- src/components/notice/notice.component.ts
+++ src/components/notice/notice.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notice',
   templateUrl: './notice.component.html',
   styleUrls: ['./notice.component.scss'],
 })
-export class NoticeComponent {}
+export class NoticeComponent implements OnInit {
+  @Input()
+  noticePhoto: string = '';
+  @Input()
+  noticeTitle: string = '';
+  @Input()
+  noticeContent: string = '';
+
+  constructor() {}
+  ngOnInit(): void {}
+}
