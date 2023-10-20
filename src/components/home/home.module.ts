560ac562ff5d954ab844eb0d4e3c65054ca8b044
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-20 09:49:22 -0300

    import notice component

--- src/components/home/home.module.ts
+++ src/components/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { MainNoticeComponent } from '../mainNotice/mainNotice.component';
+import { NoticeComponent } from '../notice/notice.component';
 
 @NgModule({
-  declarations: [HomeComponent, MainNoticeComponent],
+  declarations: [HomeComponent, MainNoticeComponent, NoticeComponent],
   imports: [CommonModule],
-  exports: [HomeComponent, MainNoticeComponent],
+  exports: [HomeComponent, MainNoticeComponent, NoticeComponent],
 })
 export class HomeModule {}
