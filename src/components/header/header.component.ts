5aabe97eac27b92fd8243d632cd40065c5c0bb81
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-20 02:07:00 -0300

    import das variáveis e funções direto no component

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: [
+    './header.component.scss',
+    '../../styles.functions.scss',
+    '../../styles.variables.scss',
+  ],
 })
 export class HeaderComponent {}
