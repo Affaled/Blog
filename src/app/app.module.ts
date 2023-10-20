ec4b875aaa3e038f025370d7d92de8f31bab0adb
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-20 02:53:36 -0300

    import home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderModule } from 'src/components/header/header.module';
+import { HomeModule } from 'src/components/home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeaderModule],
+  imports: [BrowserModule, HeaderModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
