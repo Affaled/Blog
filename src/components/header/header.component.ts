876f875c923ff6f799a3bc3081d60267104780ef
Affaled <120531449+Affaled@users.noreply.github.com>
2023-10-20 02:21:07 -0300

    remoção do import porque deu erro

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -3,10 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: [
-    './header.component.scss',
-    '../../styles.functions.scss',
-    '../../styles.variables.scss',
-  ],
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {}
